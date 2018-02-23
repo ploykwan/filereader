a20199bd78916dc21a517a623680c31a7a68e676
Kwankaew Uttama <kwankaew.u@ku.th>
2018-02-23 19:44:03 +0700

    write JavaDoc

--- src/Stopwatch/Runnable.java
+++ src/Stopwatch/Runnable.java
@@ -1,5 +1,14 @@
 package Stopwatch;
 
+/**
+ * An interface for read a file.
+ * @author kwankaew
+ *
+ */
 public interface Runnable {
+	/**
+	 * Read text file 
+	 * @return String from text
+	 */
 	public String run();
 }
